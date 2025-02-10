60c39c4fdb6057eb8dc872b7ee311272c6b7d376
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2025-02-11 02:03:03 +0530

    Update DiamondPattern.java

--- DAY 1/DiamondPattern.java
+++ DAY 1/DiamondPattern.java
@@ -15,3 +15,36 @@
 
 */
 
+import java.util.*;
+import java.util.Scanner;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       // Upper half 
+       for (int i = 1; i <= n; i++) { 
+        for (int j = 1; j <= n-i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j <= 2*i-1; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    // Lower half 
+    for (int i = n-1; i >= 1; i--) { 
+        for (int j = 1; j <= n-i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j <= 2*i-1; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    
+}
+}
+
